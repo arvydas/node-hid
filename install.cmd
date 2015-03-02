ae0d5fc62b744353678550edf4a19bc4ed8fa14d
SkyLined <berendjanwever@gmail.com>
2015-03-02 15:08:55 +0100

    Cleanup & documentation
    Add in-line documentation to install.cmd
    Add built .node files to .gitignore

--- install.cmd
+++ install.cmd
@@ -17,12 +17,15 @@ FOR /F "eol=# tokens=1,2" %%I in (build-targets.txt) DO (
       ECHO Build for %%I architecture and node v%%K.%%L.* failed.
       EXIT /B %ERRORLEVEL%
     )
+    :: Copy binary to sub-folder of "addon" folder
     IF NOT EXIST "addon\%%I\%%K.%%L" MKDIR "addon\%%I\%%K.%%L"
     COPY "build\Release\*.node" "addon\%%I\%%K.%%L\" 2>&1 >nul
     IF ERRORLEVEL 1 (
       ECHO Cannot copy build for %%I architecture and node v%%K.%%L.* to "addon\%%I\%%K.%%L\" folder.
       EXIT /B %ERRORLEVEL%
     )
+    :: Delete obsolete build folder
+    CALL node-gyp clean
   )
 )
 EXIT /B 0
