17c5ecaebfeb327a2fc6ebec8673e5428e3e80f5
SkyLined <berendjanwever@gmail.com>
2015-03-02 17:51:44 +0100

    Brute force node version
    Rather than load a single .node addon for the exact version of node, the
    code will attempt to load all .nodes for the current processor
    architecture. This allows you to only include a build for incompatible
    versions.
    
    The 0.12 versions have been removed from build-targets.txt as they are
    superfluous: the 0.11 versions load fine in 0.12.

--- addon/index.js
+++ addon/index.js
@@ -1,2 +1,15 @@
-var version = process.versions.node.match(/^(\d+\.\d+)\.\d+$/)[1];
-module.exports = require("./" + process.arch + "/" + version + "/HID.node");
+// Attempt to load each of the available .node files for the current processor
+// architecture until one succeeds, or throw an exception if none do.
+var versions = require("fs").readdirSync(__dirname + "\\" + process.arch);
+if (!versions.some(function (version) {
+  try {
+    module.exports = require("./" + process.arch + "/" + version + "/HID.node");
+    console.log(version);
+    return true;
+  } catch (e) {
+    return false;
+  }
+})) {
+  throw new Error("Your processor architecture and node version are not supported");
+};
+
